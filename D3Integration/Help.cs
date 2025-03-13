885763e3343c958f96a5c77b88647c34523c15ca
Chris <50079134+TheRealSki@users.noreply.github.com>
2025-03-13 11:27:30 -0700

    Create Help.cs
    Initial commit for the integrate application and for the Help class.

---
+++ D3Integration/Help.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace integrate
+{
+    class Help
+    {
+        #region Properties
+        public bool IsBetaAchieve { get; set; }
+        public bool IsExit { get; set; }
+        public bool IsLocal { get; set; }
+        public bool IsLog { get; set; }
+        public bool IsNoP4 { get; set; }
+        public bool IsPullAll { get; set; }
+        public bool IsNoSVN { get; set; }
+        public bool IsRunVS { get; set; }
+        public bool IsTime { get; set; }
+        public bool IsUpdateOnly { get; set; }
+        public string AuroraBuild { get; set; }
+        public string LogLocation { get; set; }
+        #endregion
+    }
+}
